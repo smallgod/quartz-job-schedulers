6f6840925655b27166d733dcadc3804e4ff9e462
jmukiibi <jonmuk7@gmail.com>
2017-01-14 22:59:20 +0300

    Jan 14th, 2016 23hr00

--- src/main/java/com/library/scheduler/CustomJobScheduler.java
+++ src/main/java/com/library/scheduler/CustomJobScheduler.java
@@ -25,6 +25,8 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerKey.triggerKey;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerKey.triggerKey;
 
 /**
  *
